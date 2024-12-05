a70f479fd7afc1b4585108ffe1fd6514f4cb3376
Oleg aka Waxmater <01eg.4ursanov@gmail.com>
2024-12-05 17:56:51 +0300

    Добавьте файлы проекта.

---
+++ WordCloudMaker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WordCloudMaker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
